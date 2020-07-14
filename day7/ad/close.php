0e9ee893d835c6158551b62010f05b7c270008f0
yxDeveloper <dilutian0415@gmail.com>
2020-07-14 22:42:56 +0800

    PHPpractice

---
+++ day7/ad/close.php
@@ -0,0 +1,8 @@
+<?php
+
+// 只要有人来请求我,意味着这个人不想看到广告
+// 我们给这个用户开张票
+setcookie('hide_ad','1');
+header('Location: index.php');
+
+?>
---
+++ day7/getcookie.php
@@ -0,0 +1,6 @@
+<?php
+
+//专门获取 cookie
+// 关联数组的方式访问客户端提交过来的 cookie
+
+var_dump($_COOKIE);
